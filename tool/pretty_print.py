334eaef0bcda5157780ad361132044dcda08bd41
marvin.fuchs <marvin.fuchs@cern.ch>
2024-07-30 17:41:17 +0200

    Add a function to print colored text

---
+++ tool/pretty_print.py
@@ -0,0 +1,33 @@
+_magenta = "\033[95m"
+_blue = "\033[94m"
+_cyan = "\033[96m"
+_green = "\033[92m"
+_orange = "\033[93m"
+_red = "\033[91m"
+_end = "\033[0m"
+_bold = "\033[1m"
+_underline = "\033[4m"
+
+
+def print_warning(message: str, end: str = '\n', flush: bool = False):
+    print(_orange + message + _end, end=end, flush=flush)
+
+
+def print_error(message: str, end: str = '\n', flush: bool = False):
+    print(_red + message + _end, end=end, flush=flush)
+
+
+def print_build_stage(message: str, end: str = '\n', flush: bool = False):
+    print(_green + message + _end, end=end, flush=flush)
+
+
+def print_build(message: str, end: str = '\n', flush: bool = False):
+    print(_cyan + message + _end, end=end, flush=flush)
+
+
+def print_clean(message: str, end: str = '\n', flush: bool = False):
+    print(_magenta + message + _end, end=end, flush=flush)
+
+
+def print_bash_output(message: str, end: str = '\n', flush: bool = False):
+    print(_blue + message + _end, end=end, flush=flush)
