39265021e52145f8d771b705bb944c371c9170f0
marvin.fuchs <marvin.fuchs@cern.ch>
2024-09-24 19:11:59 +0200

    Move configuration compiler to separate class

---
+++ tool/cfg_compiler.py
@@ -0,0 +1,208 @@
+import typing
+import sys
+import pathlib
+import json
+import jsonschema
+import yaml
+import re
+
+import pretty_print
+
+class Cfg_Compiler:
+    """
+    A class to compile the project configuration
+    """
+
+    @staticmethod
+    def _find_file(file_name: str, search_list: typing.List[pathlib.Path]) -> pathlib.Path:
+        """
+        Find file in search paths. Subdirectories are not searched.
+        
+        Args:
+            file_name:
+                Name of file to find.
+            search_list:
+                List of paths to be searched.
+
+        Returns:
+            The file that was found.
+
+        Raises:
+            FileNotFoundError: If the file could not be found.
+        """
+
+        for path in search_list:
+            # Iterate over all items in the path
+            for item in path.iterdir():
+                if item.is_file() and item.name == file_name:
+                    # Return found file
+                    return item
+
+        # Raise an exception if the file could not be found
+        raise FileNotFoundError(f'Unable to find {file_name}')
+
+
+    @staticmethod
+    def _merge_dicts(target: dict, source: dict) -> dict:
+        """
+        Recursively merge two dictionaries.
+        
+        Args:
+            target:
+                Target dictionary that receives values from the source dictionary.
+            source:
+                Source dictionary that overwrites values in the target dictionary.
+
+        Returns:
+            Merged target dictionary.
+
+        Raises:
+            None
+        """
+
+        for key, value in source.items():
+            if key in target and isinstance(target[key], dict) and isinstance(value, dict):
+                # If both values are dictionaries, merge them recursively
+                target[key] = Cfg_Compiler._merge_dicts(target[key], value)
+            elif key in target and isinstance(target[key], list) and isinstance(value, list):
+                # If both values are lists, merge them without duplicating elements
+                target[key] = list(set(target[key] + value))
+            else:
+                # If the value is not a dict or a list or if the key is not yet in the result, simply assign the value
+                target[key] = value
+
+        return target
+
+
+    @staticmethod
+    def _merge_cfg_files(config_file_name: str, socks_dir: pathlib.Path, project_dir: pathlib.Path) -> dict:
+        """
+        Recursively merge project configuration YAML files by tracing the import keys.
+        
+        Args:
+            config_file_name:
+                Name of the project configuration file to operate on.
+            socks_dir:
+                Path of the SoCks tool.
+            project_dir:
+                Path of the SoCks project.
+
+        Returns:
+            Fully assembled project configuration.
+
+        Raises:
+            None
+        """
+
+        try:
+            config_file = Cfg_Compiler._find_file(file_name=config_file_name, search_list=[socks_dir / 'project_templates', project_dir]) # ToDo: I think these paths should not be hard coded here
+        except FileNotFoundError as e:
+            print(repr(e))
+            sys.exit(1)
+
+        with config_file.open('r') as f:
+            cfg_layer = yaml.safe_load(f)
+
+        # Directly return the cfg layer if it doesn't contain an 'import' key
+        ret = cfg_layer
+
+        if 'import' in cfg_layer:
+            for file_name in cfg_layer['import']:
+                # Recursively merge the so far composed return value with the file to be imported
+                ret = Cfg_Compiler._merge_dicts(target=Cfg_Compiler._merge_cfg_files(config_file_name=file_name, socks_dir=socks_dir, project_dir=project_dir), source=ret)
+            # Remove the 'import' key from the so far composed configuration, as it is no longer needed
+            del ret['import']
+
+        return ret
+
+
+    @staticmethod
+    def _resolve_placeholders(project_cfg: dict, search_object):
+        """
+        Recursively search the project configuration and replace all placeholders.
+
+        Args:
+            project_cfg:
+                The entire project configuration.
+            search_object:
+                The part of the project configuration to be searched. The initial seed is the entire project configuration.
+
+        Returns:
+            The part of the project configuration provided in search_object with all placeholders replaced.
+
+        Raises:
+            None
+        """
+
+        if isinstance(search_object, dict):
+            # Traverse dictionary
+            for key, value in search_object.items():
+                search_object[key] = Cfg_Compiler._resolve_placeholders(project_cfg, value)
+
+        elif isinstance(search_object, list):
+            # Traverse list
+            for i, item in enumerate(search_object):
+                search_object[i] = Cfg_Compiler._resolve_placeholders(project_cfg, item)
+
+        elif isinstance(search_object, str):
+            # Replace placeholders in string, if present
+            placeholder_pattern = r'\{\{([^\}]+)\}\}'
+            # Check if one or more placeholders are present
+            if re.search(placeholder_pattern, search_object):
+                str_buffer = search_object
+                # Iterate over all placeholders
+                for path in re.findall(placeholder_pattern, search_object):
+                    keys = path.split('/')
+                    # Get value from project configuration
+                    value = project_cfg
+                    for key in keys:
+                        if key not in value:
+                            pretty_print.print_error(f'The following setting contains a placeholder that does not point to a valid setting: {search_object}')
+                            sys.exit(1)
+                        value = value[key]
+                    # Replace placeholder with value
+                    str_buffer = str_buffer.replace(f'{{{{{path}}}}}',str(value))
+                return str_buffer
+
+        # If it's neither a dict, list, nor string, return the value as-is
+        return search_object
+
+
+    @staticmethod
+    def compile(root_cfg_file: pathlib.Path, socks_dir: pathlib.Path, project_dir: pathlib.Path) -> dict:
+        """
+        Compile and validate the project configuration.
+        
+        Args:
+            root_cfg_file:
+                Path of the top level project configuration file.
+            socks_dir:
+                Path of the SoCks tool.
+            project_dir:
+                Path of the SoCks project.
+
+        Returns:
+            Fully assembled and validated project configuration.
+
+        Raises:
+            None
+        """
+
+        # Merge config files
+        project_cfg = Cfg_Compiler._merge_cfg_files(config_file_name=root_cfg_file.name, socks_dir=socks_dir, project_dir=project_dir)
+
+        # Resolve placeholders
+        project_cfg = Cfg_Compiler._resolve_placeholders(project_cfg=project_cfg, search_object=project_cfg)
+
+        # Validate project configuration
+        schema = socks_dir / 'schemas' / f'{project_cfg["project"]["type"]}.schema.json'
+        if not schema.is_file():
+            pretty_print.print_error(f'{project_cfg["project"]["type"]} is not a supported project type (Unable to find suitable schema).')
+            sys.exit(1)
+
+        with schema.open('r') as f:
+            project_cfg_schema = json.load(f)
+
+        jsonschema.validate(project_cfg, project_cfg_schema)
+
+        return project_cfg
--- tool/main.py
+++ tool/main.py
@@ -1,157 +1,12 @@
 #!/usr/bin/env python3
 
-import typing
 import sys
 import pathlib
 import importlib
-import json
-import jsonschema
-import yaml
 import copy
-import re
 
 import pretty_print
-
-
-def find_file(file_name: str, search_list: typing.List[pathlib.Path]) -> pathlib.Path:
-    """
-    Find file in search paths. Subdirectories are not searched.
-    
-    Args:
-        file_name:
-            Name of file to find.
-        search_list:
-            List of paths to be searched.
-
-    Returns:
-        The file that was found.
-
-    Raises:
-        FileNotFoundError: If the file could not be found.
-    """
-
-    for path in search_list:
-        # Iterate over all items in the path
-        for item in path.iterdir():
-            if item.is_file() and item.name == file_name:
-                # Return found file
-                return item
-    # Raise an exception if the file could not be found
-    raise FileNotFoundError(f'Unable to find {file_name}')
-
-
-def merge_dicts(target: dict, source: dict) -> dict:
-    """
-    Recursively merge two dictionaries.
-    
-    Args:
-        target:
-            Target dictionary that receives values from the source dictionary.
-        source:
-            Source dictionary that overwrites values in the target dictionary.
-
-    Returns:
-        Merged target dictionary.
-
-    Raises:
-        None
-    """
-
-    for key, value in source.items():
-        if key in target and isinstance(target[key], dict) and isinstance(value, dict):
-            # If both values are dictionaries, merge them recursively
-            target[key] = merge_dicts(target[key], value)
-        elif key in target and isinstance(target[key], list) and isinstance(value, list):
-            # If both values are lists, merge them without duplicating elements
-            target[key] = list(set(target[key] + value))
-        else:
-            # If the value is not a dict or a list or if the key is not yet in the result, simply assign the value
-            target[key] = value
-    return target
-
-
-def compose_project_configuration(config_file_name: str, socks_dir: pathlib.Path, project_dir: pathlib.Path) -> dict:
-    """
-    Recursively compose project configuration YAML files by tracing the import keys.
-    
-    Args:
-        config_file_name:
-            Name of the project configuration file to operate on.
-
-    Returns:
-        Fully assembled project configuration.
-
-    Raises:
-        None
-    """
-
-    try:
-        config_file = find_file(file_name=config_file_name, search_list=[socks_dir / 'project_templates', project_dir]) # ToDo: I think these paths should not be hard coded here
-    except FileNotFoundError as e:
-        print(repr(e))
-        sys.exit(1)
-    with config_file.open('r') as f:
-        cfg_layer = yaml.safe_load(f)
-    # Directly return the cfg layer if it doesn't contain an 'import' key
-    ret = cfg_layer
-    if 'import' in cfg_layer:
-        for file_name in cfg_layer['import']:
-            # Recursively merge the so far composed return value with the imported file 
-            ret = merge_dicts(target=compose_project_configuration(config_file_name=file_name, socks_dir=socks_dir, project_dir=project_dir), source=ret)
-        # Remove the 'import' key from the so far composed configuration, as it is no longer needed
-        del ret['import']
-    return ret
-
-
-def resolve_placeholders(project_cfg: dict, search_object):
-    """
-    Recursively search the project configuration and replace all placeholders.
-
-    Args:
-        project_cfg:
-            The entire project configuration.
-        search_object:
-            The part of the project configuration to be searched. The initial seed is the entire project configuration.
-
-    Returns:
-        The part of the project configuration provided in search_object with all placeholders replaced.
-
-    Raises:
-        None
-    """
-
-    if isinstance(search_object, dict):
-        # Traverse dictionary
-        for key, value in search_object.items():
-            search_object[key] = resolve_placeholders(project_cfg, value)
-
-    elif isinstance(search_object, list):
-        # Traverse list
-        for i, item in enumerate(search_object):
-            search_object[i] = resolve_placeholders(project_cfg, item)
-
-    elif isinstance(search_object, str):
-        # Replace placeholders in string, if present
-        placeholder_pattern = r'\{\{([^\}]+)\}\}'
-        # Check if one or more placeholders are present
-        if re.search(placeholder_pattern, search_object):
-            str_buffer = search_object
-            # Iterate over all placeholders
-            for path in re.findall(placeholder_pattern, search_object):
-                keys = path.split('/')
-                # Get value from project configuration
-                value = project_cfg
-                for key in keys:
-                    if key not in value:
-                        pretty_print.print_error(f'The following setting contains a placeholder that does not point to a valid setting: {search_object}')
-                        sys.exit(1)
-                    value = value[key]
-                # Replace placeholder with value
-                str_buffer = str_buffer.replace(f'{{{{{path}}}}}',str(value))
-            return str_buffer
-
-    # If it's neither a dict, list, nor string, return the value as-is
-    return search_object
+from cfg_compiler import Cfg_Compiler
 
 
 # Set tool and project directory
@@ -167,19 +22,7 @@ if not project_cfg_root_file.exists():
     sys.exit(1)
 
 # Get project configuration
-project_cfg = compose_project_configuration(config_file_name=project_cfg_root_file.name, socks_dir=socks_dir, project_dir=project_dir)
-project_cfg = resolve_placeholders(project_cfg, project_cfg)
-
-# Validate project configuration
-project_cfg_schema_file = socks_dir / 'schemas' / f'{project_cfg["project"]["type"]}.schema.json'
-if not project_cfg_schema_file.is_file():
-    pretty_print.print_error(f'{project_cfg["project"]["type"]} is not a supported project type.')
-    sys.exit(1)
-
-with project_cfg_schema_file.open('r') as f:
-    project_cfg_schema = json.load(f)
-
-jsonschema.validate(project_cfg, project_cfg_schema)
+project_cfg = Cfg_Compiler.compile(root_cfg_file=project_cfg_root_file, socks_dir=socks_dir, project_dir=project_dir)
 
 # Create builder objects
 builders = {}
