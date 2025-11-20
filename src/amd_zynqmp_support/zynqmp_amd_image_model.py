593ea57b5fc311a95ae127bb8d888d1861d1295f
Marvin Fuchs <marvin.fuchs@kit.edu>
2025-11-20 20:48:38 +0100

    Enable to specify the kernel image to be integrated into image.ub, BOOT.BIN, and an SD card image

--- src/amd_zynqmp_support/zynqmp_amd_image_model.py
+++ src/amd_zynqmp_support/zynqmp_amd_image_model.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field, ConfigDict, model_validator
-from typing import Optional
+from typing import Optional, Literal
 
 from abstract_builders.block_model import Block_Model, Block_Project_Model, Block_Project_Model_Default_Fields
 from amd_zynqmp_support.zynqmp_base_model import ZynqMP_Base_Model
@@ -28,6 +28,12 @@ class ZynqMP_AMD_Image_Dependencies_Model(BaseModel):
 class ZynqMP_AMD_Image_Block_Project_Model(Block_Project_Model):
     model_config = ConfigDict(extra="forbid", strict=True)
 
+    uboot_image_kernel: Literal["Image", "Image.gz"] = Field(
+        default=..., description="Kernel image to be integrated into image.ub"
+    )
+    boot_image_kernel: Literal["Image", "Image.gz", "image.ub"] = Field(
+        default=..., description="Kernel image to be integrated into BOOT.BIN or copied to the SD card image"
+    )
     size_boot_partition: int = Field(default=..., description="Size of the boot partition in MiB")
     size_rootfs_partition: int = Field(default=..., description="Size of the root file system partition in MiB")
     dependencies: ZynqMP_AMD_Image_Dependencies_Model = Block_Project_Model_Default_Fields.dependencies
